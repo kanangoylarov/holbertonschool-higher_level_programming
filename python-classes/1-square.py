32d712a8e82fa9a96038f93029ccebba581b5868
kanangoylarov <goylerov03@gmail.com>
2024-05-29 07:41:11 +0000

    lala

--- python-classes/1-square.py
+++ python-classes/1-square.py
@@ -7,15 +7,20 @@ A Square Class
 
 
 class Square:
+    """Square Class
 
-    def __init__(self, size):
-    """__init__
-
-    The __init__ method initializes the size value
-    of the square.
-
-    Attributes:
-        size (int): The size of the square.
+    A Square Class
 
     """
+
+    def __init__(self, size):
+        """__init__
+
+        The __init__ method initializes the size value
+        of the square.
+
+        Attributes:
+            size (int): The size of the square.
+
+        """
         self.__size = size
