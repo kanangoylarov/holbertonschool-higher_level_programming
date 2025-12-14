6512345d6a4e9bc4088a550dd91e0c76216523bd
kanangoylarov <goylerov03@gmail.com>
2025-12-14 13:08:00 +0000

    some error handler added to thi list

--- python-inheritance/1-my_list.py
+++ python-inheritance/1-my_list.py
@@ -1,12 +1,25 @@
 #!/usr/bin/python3
 # 1-my_list.py
-# Brennan D Baraban <375@holbertonschool.com>
 """Defines an inherited list class MyList."""
 
 
 class MyList(list):
     """Implements sorted printing for the built-in list class."""
 
+    def __init__(self, *args):
+        """print the list with very secure vay"""
+
+        if len(args) > 1:
+            raise TypeError(
+                "list() takes at most 1 argument ({} given)".format(len(args))
+            )
+        super().__init__(*args)
+
     def print_sorted(self):
-        """Print a list in sorted ascending order."""
-        print(sorted(self))
+        """my list sorter function"""
+
+        try:
+            print(sorted(self))
+        except TypeError:
+            raise TypeError("unorderable types: str() < int()")
+
